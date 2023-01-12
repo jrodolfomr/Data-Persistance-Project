ecc605208c370aca0e556d685e90e763da0931eb
jrodolfomr <jrodolfo11maci@gmail.com>
2023-01-12 14:59:31 -0800

    :sparkles: :sparkles: add more bricks
    Adds feature when breaks are all destroyed to add a new set of bricks for the player to add more points.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -22,13 +22,14 @@ public class MainManager : MonoBehaviour
     private int hiscore;
     public Text BestScoreText;
 
+    public int trackPoint = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
+        trackPoint = LineCount * perLine;
         int[] pointCountArray = new [] {1,1,2,2,5,5};
         for (int i = 0; i < LineCount; ++i)
         {
@@ -53,6 +54,24 @@ public class MainManager : MonoBehaviour
         ScoreText.text = $"{text} => Score : {m_Points}";
     }
 
+    public void ResetBoard()
+    {
+        const float step = 0.6f;
+        int perLine = Mathf.FloorToInt(4.0f / step);
+        trackPoint = LineCount * perLine;
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+        for (int i = 0; i < LineCount; ++i)
+        {
+            for (int x = 0; x < perLine; ++x)
+            {
+                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                brick.PointValue = pointCountArray[i];
+                brick.onDestroyed.AddListener(AddPoint);
+            }
+        }
+    }
+
     private void Update()
     {
         if (!m_Started)
@@ -67,6 +86,7 @@ public class MainManager : MonoBehaviour
                 Ball.transform.SetParent(null);
                 Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
             }
+
         }
         else if (m_GameOver)
         {
@@ -80,12 +100,22 @@ public class MainManager : MonoBehaviour
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
+        else
+        {
+            Debug.Log($"Pt:{trackPoint} ,pos:{ Ball.gameObject.transform.position.y}");
+            if (trackPoint <= 0 && Ball.gameObject.transform.position.y < 2f)
+            {
+                ResetBoard();
+            }
+        }
     }
 
     void AddPoint(int point)
     {
         m_Points += point;
         ScoreText.text = $"{text} => Score : {m_Points}";
+        trackPoint -= 1;
+       
     }
 
     public void GameOver()
